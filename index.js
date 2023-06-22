c9b33ccd66324f488f998b4ada448a9e2b751e9b
somaiya484 <87192408+somaiya484@users.noreply.github.com>
2023-06-22 11:50:32 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/chef', (req, res) =>{
+  res.send(chef);
+})
+
+app.get('/chef/:id' , (req, res) =>{
+  const id = req.params.id;
+  const item = chef.find (cd => cd.id == id);
+  res.send(item);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
